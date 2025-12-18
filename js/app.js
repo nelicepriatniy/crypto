19e88e270be9d64768b9ac90773af67ccb58162c
nelicepriatniy <clavik.polyakov@yandex.ru>
2025-12-18 19:07:01 +0300

    first commit

--- js/app.js
+++ js/app.js
@@ -1,84 +1,133 @@
-new WOW().init();
-const faq_items = document.querySelectorAll('.faq .item');
+document.addEventListener('DOMContentLoaded', () => {
 
-if (faq_items.length > 0) {
-  faq_items.forEach((el) => {
-    el.onclick = () => {
-      el.classList.toggle('active')
+  const preloader = document.querySelector('.preloader')
+  const percentEl = document.querySelector('.num');
+  const liveLine = document.querySelector('.line');
+
+  let percent = 0;
+
+  let startWidth = 25;
+
+  if (window.innerWidth > 600) {
+    startWidth = 25
+  } else {
+    startWidth = 45
+  }
+
+  const endWidth = 100;
+  const duration = 2000; // общая длительность анимации (мс)
+  const stepTime = 20;   // шаг обновления (мс)
+
+  const steps = duration / stepTime;
+  const percentStep = 100 / steps;
+  const widthStep = (endWidth - startWidth) / steps;
+
+  liveLine.style.width = startWidth + '%';
+
+  const interval = setInterval(() => {
+    percent += percentStep;
+
+    if (percent >= 100) {
+      percent = 100;
+      percentEl.textContent = 100;
+      liveLine.style.width = '100%';
+      clearInterval(interval);
+      setTimeout(() => {
+        preloader.classList.add('disable')
+      }, 500);
+
+      // если нужно скрывать прелоадер
+      // document.querySelector('.preloader').classList.add('hide');
+      return;
     }
-  })
-}
+
+    percentEl.textContent = Math.floor(percent);
+    liveLine.style.width = startWidth + percent * (endWidth - startWidth) / 100 + '%';
+  }, stepTime);
 
 
-const hero_form = document.querySelector('.hero form')
+  new WOW().init();
+  const faq_items = document.querySelectorAll('.faq .item');
 
-if (hero_form) {
-  const labels = hero_form.querySelectorAll('.form-label.list')
-
-  labels.forEach((label) => {
-    const input = label.querySelector('input')
-    const list_items = label.querySelectorAll('.label-list__item')
-
-    let old_value = input.value
-
-    list_items.forEach((list_item) => {
-      list_item.onclick = () => {
-        const list_item_value = list_item.innerText
-        input.value = list_item_value
-        label.classList.remove('active')
+  if (faq_items.length > 0) {
+    faq_items.forEach((el) => {
+      el.onclick = () => {
+        el.classList.toggle('active')
       }
     })
+  }
 
-    input.addEventListener('input', () => {
-      let current_value = input.value.toLowerCase()
+
+  const hero_form = document.querySelector('.hero form')
+
+  if (hero_form) {
+    const labels = hero_form.querySelectorAll('.form-label.list')
+
+    labels.forEach((label) => {
+      const input = label.querySelector('input')
+      const list_items = label.querySelectorAll('.label-list__item')
+
+      let old_value = input.value
+
       list_items.forEach((list_item) => {
-        const list_item_value = list_item.innerText.toLowerCase()
-        if (!list_item_value.includes(current_value)) {
-          list_item.classList.add('disable')
-        } else {
-          list_item.classList.remove('disable')
+        list_item.onclick = () => {
+          const list_item_value = list_item.innerText
+          input.value = list_item_value
+          label.classList.remove('active')
         }
+      })
 
-      })
-    })
+      input.addEventListener('input', () => {
+        let current_value = input.value.toLowerCase()
+        list_items.forEach((list_item) => {
+          const list_item_value = list_item.innerText.toLowerCase()
+          if (!list_item_value.includes(current_value)) {
+            list_item.classList.add('disable')
+          } else {
+            list_item.classList.remove('disable')
+          }
 
-    input.addEventListener('focus', () => {
-      list_items.forEach((el) => {
-        el.classList.remove('disable')
+        })
       })
-      labels.forEach((el) => {
-        el.classList.remove('active')
+
+      input.addEventListener('focus', () => {
+        list_items.forEach((el) => {
+          el.classList.remove('disable')
+        })
+        labels.forEach((el) => {
+          el.classList.remove('active')
+        })
+        label.classList.add('active')
+        old_value = input.value
       })
-      label.classList.add('active')
-      old_value = input.value
-    })
-    document.addEventListener('click', function (e) {
-      if (!e.target.classList.contains('label-input')) {
-        if (input.value == '') {
-          input.value = old_value
+      document.addEventListener('click', function (e) {
+        if (!e.target.classList.contains('label-input')) {
+          if (input.value == '') {
+            input.value = old_value
+          }
+          label.classList.remove('active')
+
         }
-        label.classList.remove('active')
+      })
 
+    })
+  }
+
+  const header_menu = document.querySelector('.mobile-head-menu')
+  const header_menu_close = document.querySelectorAll('.mobile-menu-close')
+  const header_menu_open = document.querySelectorAll('.header-mob-menu-open')
+
+  if (header_menu) {
+    header_menu_close.forEach((el) => {
+      el.onclick = () => {
+        header_menu.classList.remove('active')
       }
     })
+    header_menu_open.forEach((el) => {
+      el.onclick = () => {
+        header_menu.classList.add('active')
+      }
+    })
+  }
 
-  })
-}
-
-const header_menu = document.querySelector('.mobile-head-menu')
-const header_menu_close = document.querySelectorAll('.mobile-menu-close')
-const header_menu_open = document.querySelectorAll('.header-mob-menu-open')
-
-if (header_menu) {
-  header_menu_close.forEach((el) => {
-    el.onclick = () => {
-      header_menu.classList.remove('active')
-    }
-  })
-  header_menu_open.forEach((el) => {
-    el.onclick = () => {
-      header_menu.classList.add('active')
-    }
-  })
-}
-
+})
